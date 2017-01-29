3b3baa2314719e4bb3e00ddd3c4faa071c3ad1b5
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-01-29 18:27:37 +0000

    Fix bugs preventing use from node or the command line, and update README.md with usage instructions

--- generator.js
+++ generator.js
@@ -1,20 +1,26 @@
 var fs = require('fs');
+var path = require('path');
 
 module.exports = function(group, individual, quantity, type) {
 	if(arguments.length < 2) {
-		console.error("Usage: fantasynames GROUP INDIVIDUAL [QUANTITY] [TYPE]");
+		console.error("Usage: fantasy-names GROUP INDIVIDUAL [QUANTITY=1] [TYPE=0]");
 	} else {
 		if(typeof quantity === 'undefined') quantity = 1;
 		if(typeof type === 'undefined') type = 0;
 		
-		eval(fs.readFileSync('generators/'+group+'/'+individual+'.min.js','utf8'));
-		var generator = eval('generator$'+group+'$'+individual);
-		
-		var output = "";
-		for(var i = 0; i < quantity; i++) {
-			output += generator(type);
-			if(i < quantity - 1) output += '\n';
+		try {
+			eval(fs.readFileSync(path.join(__dirname, 'generators/'+group+'/'+individual+'.min.js'),'utf8'));
+			var generator = eval('generator$'+group+'$'+individual);
+			
+			var output = "";
+			for(var q = 0; q < quantity; q++) {
+				output += generator(type);
+				if(q < quantity - 1) output += '\n';
+			}
+			return output;
+		} catch(e) {
+			console.error("Couldn't load: generator$"+group+"$"+individual);
+			return "";
 		}
-		return output;
 	}
 }
